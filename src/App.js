f6a4ee1e27430b7279cdc941c5f3f7841fdf4c4b
pooya134 <38294574+pooya134@users.noreply.github.com>
2018-05-06 19:45:03 -0700

    Update App.js
    Jong's first commit for front page

--- src/App.js
+++ src/App.js
@@ -13,8 +13,38 @@ class App extends Component {
   // }
   render() {
     return (
-      <div className="App">
-      <Login/>
+      <div>
+        <title>Layout Demo</title>
+        {/* content goes here */}
+        <header>
+          <section>
+            <div className="container">
+              <label htmlFor="uname"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required />
+              <label htmlFor="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required />
+              <button type="submit">Login</button>
+              <button type="signup">Sign Up</button>
+              <label>
+                <input type="checkbox" defaultChecked="checked" name="remember" /> Remember me
+              </label>
+            </div>
+            <nav>
+              <ul>
+                <li><a href="./App.js">Home</a></li>
+                <li><a href="./search.js">Comic Search</a></li>
+                <li><a href="./user.js">User Profile</a></li>
+                <li><a href="./contact.js">Contact Us</a></li>
+              </ul>
+            </nav>
+          </section>
+        </header>
+        <main>
+        
+
+                 
+        </main>
+        <footer />
       </div>
     );
   }
