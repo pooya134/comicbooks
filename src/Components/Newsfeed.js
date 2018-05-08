df2e7558ace32437c581dbcfac2a6dd25a30e3db
pooya134 <38294574+pooya134@users.noreply.github.com>
2018-05-08 01:12:19 -0700

    update newsfeed.js
    added top menu "home" "Search" "User page"

--- src/Components/Newsfeed.js
+++ src/Components/Newsfeed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import './Login.css';
 import Comicbook from './Comicbook';
+import './style_newsfeeds.css'
 class Newsfeed extends Component {
   constructor() {
     super();
@@ -14,6 +15,18 @@ class Newsfeed extends Component {
    }
   render() {
     return (
+      <div>
+          <header>
+            <div className = "top_header"></div>
+            <nav>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Comic Search</a></li>
+                <li><a href="">User Profile</a></li>
+              </ul>
+            </nav>
+          </header>
+
       <div className="login-form">
         <h3>Sign Up</h3>
         <input className ="form"
@@ -43,6 +56,7 @@ class Newsfeed extends Component {
           })}
         </ul>
       </div>
+    </div>
     );
   }
 }
