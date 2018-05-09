c195ef06764bf71e0cf43d2c85da42686a54db45
pooya134 <38294574+pooya134@users.noreply.github.com>
2018-05-09 00:45:22 -0700

    update newsfeed.js
    border around newsfeed, search bars

--- src/Components/Newsfeed.js
+++ src/Components/Newsfeed.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 //import './Login.css';
 import Comicbook from './Comicbook';
+import './style_newsfeeds.css';
+import sign_up from './sign_up';
+
 class Newsfeed extends Component {
   constructor() {
     super();
@@ -14,35 +17,94 @@ class Newsfeed extends Component {
    }
   render() {
     return (
+      <div>
+          <header>
+            <div className = "top_header"></div>
+       
+          </header>
+      <div className = "top-menu">
+             <nav>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="./sign_up">Comic Search</a></li>
+                <li><a href="">User Profile</a></li>
+              </ul>
+            </nav>
+
+      </div>
+
       <div className="login-form">
-        <h3>Sign Up</h3>
+      <h3>Sign in</h3>
+        <p1>
         <input className ="form"
         type ="text"
         placeholder ="email"
         //onChange = {event => this.setState({email: event.target.value})}
         />
-        <p>
+        </p1>
+        <p2>
         <input className ="form"
         type ="password"
         placeholder ="password"
         //onChange = {event => this.setState({password: event.target.value})}
         />
-        </p>
-        <p>
-        <button className ="submit-button"
+        </p2>
+        <p3>
+        <button className ="sign_up"
         type ="button"
-        //onClick = {() => this.signUp()}
+        onClick = {() => this.sign_up()}
         >
-        submit
+        Sign Up
         </button>
-        </p>
-        <h5>Newsfeed</h5>
-        <ul>
-          {this.state.comics.map(comics => {
-            return <Comicbook comics= {comics}/>;
-          })}
-        </ul>
+        </p3>
+        <p4>
+        <button className = "sign-in"
+        type = "button"
+        >
+        Sign in
+        </button>
+        </p4>
+        </div>
+        <div className = "newspage">
+          <div id = "container">
+
+           <div id= "right">
+
+            <p5>
+              <input className = "search" type = "text" placeholder = "Title"/>
+            </p5>
+            <p6>
+              <input className = "search" type = "text" placeholder = "Author"/>
+            </p6>
+            <p7>
+              <input className = "search" 
+               type = "text" placeholder = "Illustrator"/>
+            </p7>
+            <p8>
+              <input className = "search" 
+               type = "text" placeholder = "Publisher"/>
+            </p8>
+            <p9>
+            <input className = "search" 
+               type = "text" placeholder = "Volume"/>
+            </p9>
+            <p10>
+              <button className ="search"
+              type ="button" Search>
+              </button>
+            </p10>
+           </div>
+
+           <div id = "left">
+            <ul>
+              {this.state.comics.map(comics => {
+              return <Comicbook comics= {comics}/>;
+              })}
+            </ul>
+        </div>
       </div>
+    </div>
+  </div>
     );
   }
 }
